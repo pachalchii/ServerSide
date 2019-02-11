99570cfdfd4c1c41070232e8a519212c720ea0fd
TheAmir <Amirhosseinvatani75@gmail.com>
2019-02-11 21:43:53 +0330

    image basecode issue solved in get product

--- Controller/AppController.js
+++ Controller/AppController.js
@@ -3,6 +3,22 @@ const bodyParser = require('body-parser');
 const {Seller , cities , sellerType , productGroups , products , sellerProducts , unit ,  car} = require('./../sequelize');
 const {colors} = require('./../Util/myVars');
 var router = express.Router();
+var path = require('path');
+const fs = require("fs");
+
+function base64_encode(file) {
+    // read binary data
+    var bitmap = fs.readFileSync(file);
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
+}
+function base64_decode(base64str, file) {
+    // create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
+    var bitmap = new Buffer(base64str, 'base64');
+    // write buffer to file
+    fs.writeFileSync(file, bitmap);
+    console.log('******** File created from base64 encoded string ********');
+}
 
 
 // get app information
@@ -30,7 +46,7 @@ router.post('/category/productGroup/product', function (req, res) {
         products.findAll({where: {
             groupid: req.body.id
             }}).then(products => {
-            res.json(products);
+                res.json(products);
 
         });
     }
@@ -43,7 +59,35 @@ router.post('/product', function (req, res) {
         sellerProducts.findAll({where: {
                 productid: req.body.id
             }}).then(products => {
-            res.json(products);
+            var final = [];
+
+            function testFunction2(value, index, array) {
+                var base64str="not Found";
+                try {
+                    base64str = base64_encode(value.image);
+
+                }catch (e) {
+                    base64str = "not Found";
+
+                }
+
+                final[index] = {
+                    id:value.id,
+                    description:value.description,
+                    price:value.price,
+                    price_date_time:value.price_date_time,
+                    supply_of_product:value.supply_of_product,
+                    unit_of_product:value.unit_of_product,
+                    productid:value.productid,
+                    sellerid:value.sellerid,
+                    unitid:value.unitid,
+                    image:base64str
+                }
+            }
+            if (products[0] != undefined){
+                products.forEach(testFunction2);
+            } else {return res.status(404).json();}
+            res.json(final);
 
         });
     }
